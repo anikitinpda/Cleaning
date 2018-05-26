ede657b6426fc02ff6c2c5210ca35183f29810ff
anikitinpda <anikitin.pda@gmail.com>
2018-05-26 13:42:12 +0300

    mod main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//плавная прокрутка страницы на якорь
 $(document).ready(function(){
  $('.header').height($(window).height());
 
@@ -5,7 +6,13 @@ $(document).ready(function(){
  	$("body,html").animate({
  		scrollTop:$("#" + $(this).data('value')).offset().top
  	},1000)
-  
  })
+})
 
+//закрывает мобильное меню после нажатия на ссылку
+$('.navbar-collapse').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+    $a = $($(this).attr('href'));
+    $('html,body').animate({ scrollTop: $a.offset().top - 50}, 500);
+    return false;
 })
