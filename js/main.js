b5f5b84d42c79d0b9a39566f61398aa6947d083c
anikitinpda <anikitin.pda@gmail.com>
2018-07-01 09:58:03 +0300

    добавил иконкку

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
  		scrollTop:$("#" + $(this).data('value')).offset().top
  	},1000)
  })
-})
 
-//закрывает мобильное меню после нажатия на ссылку
-$('.navbar-collapse').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-    $a = $($(this).attr('href'));
-    $('html,body').animate({ scrollTop: $a.offset().top - 50}, 500);
-    return false;
+ //закрывает мобильное меню после нажатия на ссылку
+ $('.navbar-collapse').on('click', function () {
+     $('.navbar-collapse').collapse('hide');
+     $a = $($(this).attr('href'));
+     $('html,body').animate({ scrollTop: $a.offset().top - 50}, 500);
+     return false;
+ })
 })
