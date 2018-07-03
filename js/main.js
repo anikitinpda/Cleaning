15f9f16de9e8a0bfe4ce66cddde2f92b56e44ae6
anikitinpda <anikitin.pda@gmail.com>
2018-07-03 18:29:52 +0300

    исправление main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
  		scrollTop:$("#" + $(this).data('value')).offset().top
  	},1000)
  })
-})
 
  //закрывает мобильное меню после нажатия на ссылку
  $('.navbar-collapse').on('click', function () {
@@ -17,16 +16,3 @@ $(document).ready(function(){
      return false;
  })
 })
-
-//плавная прокрутка по всей странице
-$('a[href*="#"]').on('click.smoothscroll', function( e ){
-var hash    = this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
-if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
-var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
-if( ! $target.length ) return;
-e.preventDefault();
-$('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 400, 'swing', function(){
-window.location.hash = hash;
-})
-})
-
